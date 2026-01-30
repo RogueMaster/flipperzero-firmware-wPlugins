a9015c97a4c2f67ab30ea6a5afd5135a31b5e366
Markus Birth <markus@birth-online.de>
2026-01-30 22:51:02 +0000

    Update About widget
    Signed-off-by: Markus Birth <markus@birth-online.de>

--- scenes/sonicare_scene_about.c
+++ scenes/sonicare_scene_about.c
@@ -1,4 +1,5 @@
 #include "../uk_mbirth_sonicare.h"
+#include "gui/canvas.h"
 #include <uk_mbirth_sonicare_icons.h>
 #include <dolphin/dolphin.h>
 
@@ -6,16 +7,12 @@ void sonicare_scene_about_on_enter(void* context) {
     Sonicare* app = context;
     Widget* widget = app->widget;
 
-    FuriString* display_text = furi_string_alloc();
-
-    furi_string_cat(display_text, "Sonicare Brush Head ID\nby mbirth.uk");
-
     widget_add_icon_element(widget, 128-17, 0, &I_sonicare_brush);
-    widget_add_text_scroll_element(widget, 0, 0, 128-17, 64, furi_string_get_cstr(display_text));
+    widget_add_string_element(widget, 0, 0, AlignLeft, AlignTop, FontPrimary, "Sonicare Brush ID");
+    widget_add_string_element(widget, 0, 12, AlignLeft, AlignTop, FontSecondary, "by mbirth.uk");
     widget_add_icon_element(widget, 8, 35, &I_sonicare_qr);
 
     view_dispatcher_switch_to_view(app->view_dispatcher, SonicareViewWidget);
-    furi_string_free(display_text);
 }
 
 bool sonicare_scene_about_on_event(void* context, SceneManagerEvent event) {
