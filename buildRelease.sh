79782bbfbe4cb691183f257be1843d114cd61b55
RogueMaster <RogueMaster@users.noreply.github.com>
2026-06-30 12:40:43 -0400

    Latest RM0630-0125-0.420.0-476bafc on PATREON & GitHub - 420 UPDATED
    Add executable file

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,13 +1,12 @@
-rm -rf RM*-*-*.tgz RM*-*-*.zip .sconsign.dblite dist build build/f7-firmware-C/resources/apps build/f7-firmware-C/resources/nfc/RMProTrainedAmiibo
+rm -rf RM*-*-*.tgz RM*-*-*.zip .sconsign.dblite dist build
 git pull
 DATE_VAR=`date +%m%d`
 TIME_VAR=`date +%H%M`
-VER_VAR=`cat scripts/version.py | grep "or \"0" | awk -F\" '{print $4}'`
-HASH_VAR=`git rev-parse \`git branch -r --sort=committerdate | tail -1\` | awk '{print substr($0,1,7)}' | tail -1`
+HASH_VAR=`git rev-parse \`git branch -r --sort=committerdate | tail -1\` | awk '{print substr($0,1,8)}' | tail -1`
 ./fbt updater_package
-mv dist/f7-C/f7-update-RM420FAP "RM$DATE_VAR-$TIME_VAR"
-zip -rq "RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "RM$DATE_VAR-$TIME_VAR"
-tar -czf "RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "RM$DATE_VAR-$TIME_VAR"
-rm -rf "RM$DATE_VAR-$TIME_VAR"
+cp -r "dist/f7-C/f7-update-rm-420-$HASH_VAR" "RM$DATE_VAR-$TIME_VAR-$HASH_VAR"
+zip -rq "RM$DATE_VAR-$TIME_VAR-$HASH_VAR.zip" "RM$DATE_VAR-$TIME_VAR-$HASH_VAR"
+tar -czf "RM$DATE_VAR-$TIME_VAR-$HASH_VAR.tgz" "RM$DATE_VAR-$TIME_VAR-$HASH_VAR"
+rm -rf "RM$DATE_VAR-$TIME_VAR-$HASH_VAR"
 git stash
-echo "BUILD COMPLETED, ZIP AND TGZ GENERATED FOR RM$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR"
+echo "BUILD COMPLETED, ZIP AND TGZ GENERATED FOR RM$DATE_VAR-$TIME_VAR-$HASH_VAR"
