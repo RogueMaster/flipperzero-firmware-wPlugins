5632e7e5aef2f891b7086c243b9347fb6ed7e530
Richard <yo3gnd.dev.493@yo3gnd.ro>
2026-08-02 07:41:50 +0300

    generating some tones

--- src/fmtx_audio.h
+++ src/fmtx_audio.h
@@ -3,6 +3,19 @@
 
 #include <stdint.h>
 
+typedef struct
+{
+    uint32_t rp;
+    uint32_t cp;
+    uint32_t ri;
+    uint32_t ci;
+    uint16_t tone;
+    uint16_t gap;
+    uint8_t key;
+} Dtmf;
+
 int16_t u8pcm(uint8_t s);
+void dtmfinit(Dtmf *dtmf);
+int16_t dtmfnext(Dtmf *dtmf);
 
 #endif
