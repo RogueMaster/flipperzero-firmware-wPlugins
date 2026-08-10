526e8267f6d31eac7924d8ddedfa3aa94717f2a2
Miguel Ángel Antolín Bermúdez <chicoantolin@gmail.com>
2026-08-10 22:58:59 +0200

    fix: remove trailing backslash in maze3d.h comment (multi-line comment -Werror fix)

--- applications/external/maze_3d/maze3d.h
+++ applications/external/maze_3d/maze3d.h
@@ -447,5 +447,5 @@ const char* story_title(int story_id);
 
 extern const uint8_t TEXTURES[][8];
 #define TEX_COUNT \
-    24 // v6.7-beta: 扩展到24槽, 保留12个空白给未来方块 \
+    24 // v6.7-beta: 扩展到24槽, 保留12个空白给未来方块
         // (圆石/煤/铁/钻石/台阶/玻璃/工作台/熔炉/羊毛/苔石/地狱岩/末地石)
