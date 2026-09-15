b6da8da01e27baaec45f1ee8b10b2a16c4fc617a
Tyler Anderson <130514297+Tyl3rA@users.noreply.github.com>
2026-09-14 18:09:08 -0600

    Remove writing to /ext/flip_crypt_saved/

--- storage.c
+++ storage.c
@@ -5,9 +5,9 @@ void save_result(const char* text, char* file_name) {
     File* file = storage_file_alloc(furi_record_open(RECORD_STORAGE));
 
     char buffer[128];
-    snprintf(buffer, sizeof(buffer), "/ext/flip_crypt_saved/%s.txt", file_name);
-    
-    if(storage_simply_mkdir(furi_record_open(RECORD_STORAGE), "/ext/flip_crypt_saved")) {
+    snprintf(buffer, sizeof(buffer), FLIP_CRYPT_DATA_DIR "/saved/%s.txt", file_name);
+
+    if(storage_simply_mkdir(furi_record_open(RECORD_STORAGE), FLIP_CRYPT_DATA_DIR "/saved")) {
         if (storage_file_open(file, buffer, FSAM_WRITE, FSOM_CREATE_ALWAYS)) {
             storage_file_write(file, text, strlen(text));
         }
